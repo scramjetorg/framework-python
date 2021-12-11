61758cf9251088db902eacfab4b081717a823375
Michal Piotrowski <piotrowskimichalwfis@gmail.com>
2021-12-11 19:26:22 +0100

    Add setup.py script for packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="scramjet-framework-python",
+    version='0.1',
+    author="Scramjet.org",
+    author_email="<info@scramjet.org>",
+    description='Scramjet is a simple reactive stream programming framework.',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'streams'],
+    classifiers=[
+    ]
+)
